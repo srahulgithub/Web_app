f83e85455300bc67d6995d3d4c6f902d946ae7d4
srahulgithub <75783819+srahulgithub@users.noreply.github.com>
2023-01-11 17:35:31 +0530

    Add files via upload

---
+++ Models/details.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+namespace WebApplication1.Models
+{
+    public class details
+    {
+        public String Id { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Ip_address { get; set; }
+        
+    }
+}
