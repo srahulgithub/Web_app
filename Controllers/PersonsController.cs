edcf0e709d375cec9dd7cfd522780257e2f32bd4
srahulgithub <75783819+srahulgithub@users.noreply.github.com>
2023-01-18 18:01:52 +0530

    Add files via upload

---
+++ Controllers/PersonsController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project2.Services;
+
+namespace Project2.Controllers
+{
+    public class PersonsController : Controller
+    {
+
+        private readonly PersonService personService;
+
+        public PersonsController(PersonService personService)
+        {
+            this.personService = personService;
+        }
+
+        // GET: PersonsController
+        public ActionResult Index()
+        {
+            return View(personService.Get());
+        }
+
+        // GET: PersonsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PersonsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PersonsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PersonsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PersonsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,23 @@
+﻿//using Project2.CustomAttributes;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project2.Models
+{
+    public class Person
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [BsonElement("Country")]
+        [Required]
+        public string Country { get; set; }
+
+    }
+}
